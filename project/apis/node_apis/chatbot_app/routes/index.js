90b8d3203bbb82ad50d4e3264f33ff0312c08c17
Neil Thaker <neilthaker07@gmail.com>
2018-10-12 23:06:02 -0700

    exception handling in apis.

--- project/apis/node_apis/chatbot_app/routes/index.js
+++ project/apis/node_apis/chatbot_app/routes/index.js
@@ -22,64 +22,88 @@ router.get('/answer/:intent/:entity', function(req, res, next) {
 
 	var mongoClient = require('mongodb').MongoClient;
 	var url = "mongodb://adminUser:purveshFALL2018@13.58.23.159/?authSource=admin&authMechanism=SCRAM-SHA-1";
+	//var url = "mongodb://localhost:27017";
 
-	mongoClient.connect(url, function(err, db) {
-	  if (err) throw err;
-	  var dbo = db.db(dbName);
-	  var collection = dbo.collection( collectionName );
-
-	  collection.find({[String(intentInput)] : {$exists: true}}).toArray(function(err, result){
-	    if (err) throw err;
-
-		var answer = result[0][intentInput][entityInput];
-		console.log("Answer : "+answer);
-		hasImage = false;
-
-		// Image fetch on the front end directly
-		if(intentInput.includes('ui_') && answer!==null && answer !=='')
-		{
-			hasImage = true;
-			// gridfs
-			/*mongoose.connect('mongodb://localhost:27017/'+dbName); // proper ip name of EC2
-			var conn = mongoose.connection;
-
-			Grid.mongo = mongoose.mongo;
-			
-			conn.once('open', function(){
-
-				var gfs = Grid(conn.db);
-				var writestream = fs.createWriteStream(path.join(__dirname, '/'+answer));
-				try {
-
-					var readstream = gfs.createReadStream({
-						_id: answer
-					});
-
-					readstream.pipe(writestream, function(){
-						res.sendFile(path.join(__dirname, '/'+answer));	
-					});
-					//writestream.on('close', function(){
-					//	console.log(" written new file image - ");
-					//});
-
-				} 
-				catch (err) {
-					log.error("image not found....");
-				    log.error(err);
-				}
-
-			});*/
-		}
+	mongoClient.connect(url, function(err1, db) {
 		
-		res.json({
-		    answer: answer,
-		    image: hasImage
-	  	});
+	  	try
+		{
+			var dbo = db.db(dbName);
+			var collection = dbo.collection( collectionName );
 
-	    db.close();
-	  });
+			collection.find({[String(intentInput)] : {$exists: true}}).toArray(function(err2, result)
+			{
+			  	try
+				{ 	
+					var hasImage = false;
+					var answer = result[0][intentInput][entityInput];
+					if(typeof answer === 'undefined') // In case entity not found.
+					{
+						answer = "";
+					}
+
+					console.log("Answer : "+answer);
+
+					// Image fetch on the front end directly
+					if(intentInput.includes('ui_') && answer!==null && answer !=='')
+					{
+						hasImage = true;
+						// gridfs code to save image is below.
+					}
+
+					res.json({
+						answer: answer,
+						image: hasImage
+					});
+				}
+				catch (err2) 
+				{
+					console.log("No Intent found in database.----------");
+				    console.log(err2);
+				    res.send(500, {error:"No Intent found in database."});
+				}
+			});
+
+		}
+		catch (err1) 
+		{
+			console.log("Mongodb Connection Issue.----------");
+		    console.log(err1);
+		    res.send(500, {error:"Mongodb Connection Issue."});
+		}
 
 	});
 });
 
 module.exports = router;
+
+// gridfs
+/*mongoose.connect('mongodb://localhost:27017/'+dbName); // proper ip name of EC2
+var conn = mongoose.connection;
+
+Grid.mongo = mongoose.mongo;
+
+conn.once('open', function(){
+
+	var gfs = Grid(conn.db);
+	var writestream = fs.createWriteStream(path.join(__dirname, '/'+answer));
+	try {
+
+		var readstream = gfs.createReadStream({
+			_id: answer
+		});
+
+		readstream.pipe(writestream, function(){
+			res.sendFile(path.join(__dirname, '/'+answer));	
+		});
+		//writestream.on('close', function(){
+		//	console.log(" written new file image - ");
+		//});
+
+	} 
+	catch (err) {
+		log.error("image not found....");
+	    log.error(err);
+	}
+
+});*/
