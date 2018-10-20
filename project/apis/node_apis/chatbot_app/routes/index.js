de821000cc0152c40495724a46fc4cf15d28daac
Neil Thaker <neilthaker07@gmail.com>
2018-10-20 15:01:54 -0700

    url change

--- project/apis/node_apis/chatbot_app/routes/index.js
+++ project/apis/node_apis/chatbot_app/routes/index.js
@@ -21,8 +21,8 @@ router.get('/answer/:intent/:entity', function(req, res, next) {
 	var collectionName = "intent";
 
 	var mongoClient = require('mongodb').MongoClient;
-	var url = "mongodb://adminUser:purveshFALL2018@13.58.23.159/?authSource=admin&authMechanism=SCRAM-SHA-1";
-	//var url = "mongodb://localhost:27017";
+	//var url = "mongodb://adminUser:purveshFALL2018@13.58.23.159/?authSource=admin&authMechanism=SCRAM-SHA-1";
+	var url = "mongodb://localhost:27017";
 
 	mongoClient.connect(url, function(err1, db) {
 		
