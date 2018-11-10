76bda9b2f8f90228cc1dc80c38ca42e4251d35e7
Purvesh Kothari <purvesh2310@gmail.com>
2018-11-10 12:50:52 -0800

    Fixed the issue of infinite button click loop and added experimental audio recoding code

--- project/apis/node_apis/webclient/routes/conversation.js
+++ project/apis/node_apis/webclient/routes/conversation.js
@@ -1,5 +1,8 @@
 // Instantiate a DialogFlow client.
 const dialogflow = require('dialogflow');
+const pump = require('pump');
+const record = require('node-record-lpcm16');
+const through2 = require('through2');
 const sessionClient = new dialogflow.SessionsClient();
 
 // You can find your project ID in your Dialogflow agent settings
@@ -13,7 +16,102 @@ const sessionPath = sessionClient.sessionPath(projectId, sessionId);
 var express = require('express');
 var router = express.Router();
 
+router.post('/voice',function(req,res){
 
+  //testFun();
+
+  res.send({ "Hello":"world" });
+
+});
+
+function testFun() {
+
+  const initialStreamRequest = {
+    session: sessionPath,
+    queryParams: {
+      session: sessionClient.sessionPath(projectId, sessionId),
+    },
+    queryInput: {
+      audioConfig: {
+        audioEncoding: encoding,
+        sampleRateHertz: sampleRateHertz,
+        languageCode: languageCode,
+      },
+      singleUtterance: true,
+    },
+  };
+
+  // Create a stream for the streaming request.
+  const detectStream = sessionClient
+    .streamingDetectIntent()
+    .on('error', console.error)
+    .on('data', data => {
+      if (data.recognitionResult) {
+        console.log(
+          `Intermediate transcript: ${data.recognitionResult.transcript}`
+        );
+      } else {
+        console.log(`Detected intent:`);
+        logQueryResult(sessionClient, data.queryResult);
+      }
+  });
+
+  // Write the initial stream request to config for audio input.
+  detectStream.write(initialStreamRequest);
+
+  pump(
+      record
+        .start({
+          sampleRateHertz: '44100',
+          threshold: 0.5,
+          verbose: false,
+          recordProgram: 'rec',
+          silence: '1.0',
+        })
+        .on('error', console.error),
+      // Format the audio stream into the request format.
+      through2.obj((obj, _, next) => {
+        next(null, {inputAudio: obj});
+      }),
+      detectStream
+    );
+    // [END dialogflow_detect_intent_streaming]
+
+}
+
+function logQueryResult(sessionClient, result) {
+
+  // Instantiates a context client
+  const contextClient = new dialogflow.ContextsClient();
+
+  console.log(`  Query: ${result.queryText}`);
+  console.log(`  Response: ${result.fulfillmentText}`);
+  if (result.intent) {
+    console.log(`  Intent: ${result.intent.displayName}`);
+  } else {
+    console.log(`  No intent matched.`);
+  }
+  const parameters = JSON.stringify(
+    structjson.structProtoToJson(result.parameters)
+  );
+  console.log(`  Parameters: ${parameters}`);
+
+  if (result.outputContexts && result.outputContexts.length) {
+    console.log(`  Output contexts:`);
+    result.outputContexts.forEach(context => {
+      const contextId = contextClient.matchContextFromContextName(context.name);
+      const contextParameters = JSON.stringify(
+        structjson.structProtoToJson(context.parameters)
+      );
+      console.log(`    ${contextId}`);
+      console.log(`      lifespan: ${context.lifespanCount}`);
+      console.log(`      parameters: ${contextParameters}`);
+    });
+  }
+}
+
+
+// Detecting Intent from the Text
 router.post('/ask',function(req,res){
 
   var userQuery = req.body.userQuery;
@@ -30,6 +128,7 @@ router.post('/ask',function(req,res){
   };
 
   var answerFromServer = "";
+  var intent = "";
 
   // Send request and log result
   sessionClient.detectIntent(request)
@@ -42,12 +141,13 @@ router.post('/ask',function(req,res){
       console.log(`  Response: ${result.fulfillmentText}`);
 
       if (result.intent) {
+        intent = result.intent.displayName
         console.log(`  Intent: ${result.intent.displayName}`);
       } else {
         console.log(`  No intent matched.`);
       }
 
-      res.send(answerFromServer);
+      res.send({ answerText: answerFromServer, intentName: intent });
 
     })
     .catch(err => {
