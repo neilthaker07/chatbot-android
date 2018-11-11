e154a4095ff76337248cb5571cad91a804ae4bfe
Neil Thaker <neilthaker07@gmail.com>
2018-11-11 10:58:34 -0800

    reverting data insert image script

--- project/scripts/data_insert_image.py
+++ project/scripts/data_insert_image.py
@@ -13,12 +13,13 @@ except:
 db = conn.Assistant_Chatbot_Android_Developer 
   
 # Created or Switched to collection names: intent
-collection = db.ui_element
+collection = db.intent
 
 imagePath = '/home/neil/Neil_Work/MS_SJSU/295A/ui_component_repo/awesome-android-ui'
 fileNames=['ui_actionbar','ui_animation','ui_button','ui_calendar','ui_dialog','ui_effect','ui_graph','ui_image','ui_label','ui_layout','ui_list','ui_material','ui_menu','ui_other','ui_parallax','ui_progress','ui_seekbar','ui_viewpager']
 
 intent = {}
+intent['ui_element']={}
 
 j=0
 for i in range(len(fileNames)):
@@ -26,7 +27,7 @@ for i in range(len(fileNames)):
     data_in_list=[]
 
     #  /home/neil/Neil_Work/MS_SJSU/295A/ui_component_repo/awesome-android-ui
-    with open('../data/data-'+fileNames[i]+'.csv', 'r') as dataFile:
+    with open('../data/data-'+fileNames[i]+'.csv', 'rb') as dataFile:
         reader = csv.reader(dataFile)
         data_in_list = list(reader)
 
@@ -37,9 +38,9 @@ for i in range(len(fileNames)):
     for each_row in data_in_list:
         vals = (str(each_row)).strip().split('|')
         entity = vals[0].split(']')[0].split('[')[2]
-        entity = entity.replace(".", " ")
-        #entity = entity.translate(None, '.')
-        #entity = entity.strip()
+        #entity = entity.replace(".", " ")
+        entity = entity.translate(None, '.')
+        entity = entity.strip()
 
         #print(entity)
 
@@ -75,7 +76,7 @@ for i in range(len(fileNames)):
                 with open(imagePath, 'rb') as imageFile:
                     putId = fs.put(imageFile)
                     #putId = fs.put(imagePath) # saving image using gridfs, returns id of saved image
-                    intent[entity] = putId # saved image id as a value of entity
+                    intent['ui_element'][entity] = putId
                     print("id of image ----------------------------------")
                     print(entity)
                     print(putId)
